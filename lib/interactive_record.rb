170978da4ce9d51379705a04f2e68765dcdfbe21
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-05-20 01:45:11 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,5 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-require 'pry'
 
 class InteractiveRecord
 
@@ -9,45 +8,56 @@ class InteractiveRecord
   end
 
   def self.column_names
-    sql="PRAGMA table_info(#{self.table_name})"
-    columns=DB[:conn].execute(sql)
-    #binding.pry
-    column_names=[]
-    columns.each do |column|
-      #binding.pry
-      column_names << column["name"]
+    #hash of columns names
+    #return them as a an array of strings
+    column_names = []
+    #DB[:conn].results_as_hash = true #not sure that this is needed since it is in the environment file
+
+    sql = "PRAGMA table_info('#{table_name}')" #give you the hash of info
+    table_info = DB[:conn].execute(sql)
+
+    table_info.each do |col| #iterate over the array of hashes
+      column_names << col["name"] #this gives you the value for the key "name"
     end
-    column_names
+    column_names.compact #to get rid of any nulls
   end
 
-  def initialize(options={})
-    options.each do |property,value|
-      self.send("#{property}=", value)
+
+  def initialize(options = {}) #pass in a hash
+    options.each do |key,value|
+      self.send(("#{key}="),value)
     end
   end
 
   def table_name_for_insert
+    #returns the table name when called on an intance of a Student
     self.class.table_name
   end
 
   def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+    #return the column names when called on an instance of Student
+    #returns it as a string ready to be inserted into a sql statement
+    self.class.column_names.delete_if {|column_name| column_name == "id"}.join (", ")
   end
 
   def values_for_insert
-    values = []
-     self.class.column_names.each do |col_name|
-       values << "'#{send(col_name)}'" unless send(col_name).nil?
-     end
-     values.join(", ")
+    #to insert data into db
+    #formats the column names to be used in a SQL statement
+    #use the column_names array, iterate over it to get the attribute names
+    #and then user the attribute = method with send to assign the value
+    values_array = []
+    self.class.column_names.each do |column_name|
+      #get the value for each attribute name
+      values_array << "'#{send(column_name)}'" unless send(column_name).nil?
+    end
+    values_array.join(", ")
   end
 
   def save
+    #insert data into db #save saves the student to the db
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-
     DB[:conn].execute(sql)
-
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
   def self.find_by_name(name)
@@ -55,9 +65,28 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(options={})
-    sql="SELECT * FROM #{self.table_name} WHERE #{options.keys[0]}=?"
-    DB[:conn].execute(sql,options[options.keys[0].to_sym])
+  def self.find_by(attribute)
+    #executes the SQL to find a row by the attribute passed into the method
+    #WHERE name = ? OR grade = ? OR id = ?
+    #attribute is a hash, so it has a key/value pair
+    attribute_key = attribute.keys.join()
+    attrubute_value = attribute.values.first
+    sql =<<-SQL
+      SELECT * FROM #{self.table_name}
+      WHERE #{attribute_key} = "#{attrubute_value}"
+      LIMIT 1
+    SQL
+    row = DB[:conn].execute(sql)
   end
+  
+=begin
+this was the schools answer
+ def self.find_by(attribute_hash)
+    value = attribute_hash.values.first
+    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+    DB[:conn].execute(sql)
+  end
+=end
 
 end
