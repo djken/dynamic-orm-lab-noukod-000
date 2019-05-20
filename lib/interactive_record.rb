580f8efd79b325d42eac09e32b29b94e0b43c709
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-05-20 01:43:00 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -9,37 +9,21 @@ class InteractiveRecord
   end
 
   def self.column_names
-    DB[:conn].results_as_hash = true
-
-    table_columns = DB[:conn].execute("PRAGMA table_info(#{table_name})")
-    column_names = []
-
-    table_columns.each do |col|
-      column_names << col["name"]
+    sql="PRAGMA table_info(#{self.table_name})"
+    columns=DB[:conn].execute(sql)
+    #binding.pry
+    column_names=[]
+    columns.each do |column|
+      #binding.pry
+      column_names << column["name"]
     end
-
-    column_names.compact
+    column_names
   end
 
-  def initialize(objects={})
-    objects.each do |k, v|
-      self.send("#{k}=", v)
+  def initialize(options={})
+    options.each do |property,value|
+      self.send("#{property}=", value)
     end
-  end
-
-  # ORM Methods
-
-  # SAVE - INSERT INTO students(name, grade) VALUES (x, y)
-
-  def save
-    sql = <<-SQL
-      INSERT INTO #{table_name_for_insert}(#{col_names_for_insert})
-      VALUES (#{values_for_insert})
-      SQL
-
-    DB[:conn].execute(sql)
-
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
   def table_name_for_insert
@@ -52,32 +36,28 @@ class InteractiveRecord
 
   def values_for_insert
     values = []
+     self.class.column_names.each do |col_name|
+       values << "'#{send(col_name)}'" unless send(col_name).nil?
+     end
+     values.join(", ")
+  end
 
-    self.class.column_names.each do |col_name|
-      values << "'#{send(col_name)}'" unless send(col_name).nil?
-    end
-    values.join(", ")
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+
+    DB[:conn].execute(sql)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT * FROM #{table_name}
-      WHERE name = ?
-      SQL
-
-    DB[:conn].execute(sql, name);
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
   end
 
-  def self.find_by(attribute)
-    column_name = attribute.keys[0].to_s
-    value_name = attribute.values[0]
-
-    sql = <<-SQL
-      SELECT * FROM #{table_name}
-      WHERE #{column_name} = ?
-      SQL
-
-    DB[:conn].execute(sql, value_name);
+  def self.find_by(options={})
+    sql="SELECT * FROM #{self.table_name} WHERE #{options.keys[0]}=?"
+    DB[:conn].execute(sql,options[options.keys[0].to_sym])
   end
 
 end
