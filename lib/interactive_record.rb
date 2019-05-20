6f14192b6ea084eac422e8d335593f2e40ec58f6
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-05-20 01:54:56 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,5 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
 
@@ -8,17 +9,18 @@ class InteractiveRecord
   end
 
   def self.column_names
-    DB[:conn].results_as_hash = true
-    sql = "pragma table_info('#{table_name}')"
-    columns = DB[:conn].execute(sql)
-    column_arr = []
+    sql="PRAGMA table_info(#{self.table_name})"
+    columns=DB[:conn].execute(sql)
+    #binding.pry
+    column_names=[]
     columns.each do |column|
-      column_arr << column["name"]
+      #binding.pry
+      column_names << column["name"]
     end
-    column_arr
+    column_names
   end
 
-  def initialize(options = {})
+  def initialize(options={})
     options.each do |property,value|
       self.send("#{property}=", value)
     end
@@ -29,20 +31,22 @@ class InteractiveRecord
   end
 
   def col_names_for_insert
-    self.class.column_names.delete_if{|c| c == "id"}.join(", ")
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
   def values_for_insert
     values = []
-    self.class.column_names.each do |col_name|
-      values << "'#{send(col_name)}'" unless send(col_name).nil?
-    end
-    values.join(", ")
+     self.class.column_names.each do |col_name|
+       values << "'#{send(col_name)}'" unless send(col_name).nil?
+     end
+     values.join(", ")
   end
 
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+
     DB[:conn].execute(sql)
+
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
@@ -51,10 +55,9 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(option={})
-    sql = "SELECT * FROM #{self.table_name} WHERE #{option.keys[0].to_s} = '#{option.values[0].to_s}'"
-    DB[:conn].execute(sql)
+  def self.find_by(options={})
+    sql="SELECT * FROM #{self.table_name} WHERE #{options.keys[0]}=?"
+    DB[:conn].execute(sql,options[options.keys[0].to_sym])
   end
-
 
 end
